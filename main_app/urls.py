df53245977ab08b1d8e4e1998a6e6e7d50349057
Yi Chien Lee <yician.lee@gmail.com>
2024-05-01 11:43:55 -0700

    add url, model, template for user auth

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,9 +6,9 @@ urlpatterns = [
     path('playlists/', views.playlists_index, name='index'),
     path('playlists/<int:playlist_id>/', views.playlists_detail, name='detail'),
     path('playlists/create', views.PlaylistCreate.as_view(), name='playlists_create'),
-    path('playlists/<int:playlist_id>/update/', views.PlaylistUpdate.as_view(), name='playlists_update'),
+    path('playlists/<int:pk>/update/', views.PlaylistUpdate.as_view(), name='playlists_update'),
     path('songs/', views.songs, name='song'),
     path('playlists/<int:playlist_id>/save', views.playlist_songsaving, name='playlist_songsaving'),
     path('playlists/<int:playlist_id>/delete/songs/<str:spotify_id>', views.song_delete, name='songs_delete'),
-    path('playlists/<int:playlist_id>/delete/', views.PlaylistDelete.as_view(), name='playlists_delete'),
+    path('playlists/<int:pk>/delete/', views.PlaylistDelete.as_view(), name='playlists_delete'),
 ]
